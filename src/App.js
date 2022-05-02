3cc27950fb0b3a49863f07a655d257f31d3df29f
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-02 03:00:38 +0000

    Update App state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import { Nav } from './components/Nav';
 import Searchbar from './components/Searchbar';
 import { Weather } from './components/Weather';
+import { useState } from 'react';
 
 function App() {
+  const [location, setLocation] = useState('Accra')
+
   return (
     <div className="App">
       <Nav />
-      <Searchbar />
-      <Weather />
+      <Searchbar action={setLocation} />
+      <Weather location = {location} />
     </div>
   );
 }
