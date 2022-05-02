c61913ccd593914ef4907485b11076ee8aa2f37e
bentilshadrack72@gmail.com <Bentilshadrack72@gmail.com>
2022-05-02 00:34:20 +0000

    Initalize main weather compoent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 import Searchbar from './components/Searchbar';
+import { Weather } from './components/Weather';
 
 function App() {
   return (
     <div className="App">
       <Searchbar />
+      <Weather />
     </div>
   );
 }
---
+++ src/components/Weather/index.js
@@ -0,0 +1,7 @@
+import './style.css'
+
+export const Weather = () => {
+    return (
+        <div className='main'>Hi</div>
+    )
+}
